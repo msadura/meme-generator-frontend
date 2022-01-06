8a690df8a6b2669c75b1ebb8072a5bb5cde4e2eb
Marek Sadura <m.sadura@outlook.com>
2022-01-06 10:27:44 +0100

    Generated meme modal style

--- src/components/MemeGeneratedModal/MemeGeneratedModal.tsx
+++ src/components/MemeGeneratedModal/MemeGeneratedModal.tsx
@@ -4,12 +4,31 @@ import Img from 'next/image';
 import { useMeme } from '@app/components/MemeProvider/MemeProvider';
 import Button from '@app/components/Button/Button';
 import Link from 'next/link';
+import Logo2Img from '@public/dac-logo-border.png';
+import OpenseaImg from '@public/opensea.svg';
+import CopyImg from '@public/copy.svg';
+import Image from 'next/image';
+import { MEME_PATH, OPENSEA_BASE } from '@app/constants';
+import { toast } from 'react-toastify';
+import { CONTRACTS } from '@app/addresses';
 
 export function MemeGeneratedModal(): JSX.Element | null {
   const [isOpen, setIsOpen] = useState(false);
   const { getImageUrl, setBackgroundImg, bgImg } = useCanvas();
   const { lastMintedId } = useMeme();
   const img = getImageUrl();
+  const [memeUrl, setMemeUrl] = useState('');
+  const openseaUrl = `${OPENSEA_BASE}${CONTRACTS.nft}/${lastMintedId}`;
+
+  const copyShareUrl = async (url: string) => {
+    try {
+      // This will work only on https:// !!
+      await navigator.clipboard.writeText(url);
+      toast.success('Link copied to clipboard');
+    } catch (e) {
+      toast.error('Could not copy link.');
+    }
+  };
 
   const makeAnother = () => {
     setBackgroundImg(null);
@@ -17,6 +36,8 @@ export function MemeGeneratedModal(): JSX.Element | null {
 
   useEffect(() => {
     if (lastMintedId && img && bgImg) {
+      const mUrl = `${window?.location.origin}/${MEME_PATH}${lastMintedId}`;
+      setMemeUrl(mUrl);
       setIsOpen(true);
     } else {
       setIsOpen(false);
@@ -36,16 +57,88 @@ export function MemeGeneratedModal(): JSX.Element | null {
   }
 
   return (
-    <div className="fixed z-50 w-screen h-screen overflow-y-auto top-0 left-0 backdrop">
-      <div className="flex flex-1 items-center justify-center">
-        <div className="flex flex-col bg-base-200 p-5 pt-20 mt-20 rounded-md shadow-lg self-center max-w-xl">
-          <div className="flex flex-1 relative">
+    <div className="fixed z-50 w-full h-full overflow-y-auto top-0 left-0 backdrop">
+      <div className="m-3">
+        <div className="flex flex-col bg-base-200 p-3 md:p-8 mt-20 rounded-md shadow-lg mx-auto max-w-xl">
+          <div className="text-xl font-salt items-center justify-center p-3 pb-5">
+            <p className="text-center">Nice piece of art!</p>
+          </div>
+          <div className="flex flex-1 relative items-center justify-center">
             <Img src={img} width={bgImg?.width} height={bgImg?.height} />
           </div>
+
+          <div className="text-lg text-secondary italic items-center justify-center mt-3">
+            <p className="">Share it with other doges:</p>
+          </div>
+
+          <div className="flex flex-col mt-3 mb-3 gap-3">
+            <div className="flex flex-col md:flex-row flex-1 gap-0 md:gap-3">
+              <div className="flex flex-row items-center gap-3">
+                <div className="relative flex w-10">
+                  <Image src={Logo2Img} alt="DAC" />
+                </div>
+                <div className="flex w-24">Meme link:</div>
+              </div>
+
+              <div className="flex flex-row flex-1 items-center truncate">
+                <div className="flex flex-1 truncate">
+                  <a
+                    className="link link-primary truncate min-w-0"
+                    href={memeUrl}
+                    target="_blank"
+                    rel="noreferrer">
+                    {memeUrl}
+                  </a>
+                </div>
+
+                <div data-tip="Copy meme link" className="tooltip tooltip-left">
+                  <Button
+                    class="btn btn-square btn-accent text-primary-content"
+                    onClick={() => copyShareUrl(memeUrl)}>
+                    <div className="relative w-5 h-5">
+                      <Image src={CopyImg} alt="copy" layout="fill" />
+                    </div>
+                  </Button>
+                </div>
+              </div>
+            </div>
+
+            <div className="flex flex-col md:flex-row flex-1 gap-0 md:gap-3">
+              <div className="flex flex-row items-center gap-3">
+                <div className="relative flex w-10">
+                  <Image src={OpenseaImg} alt="Opensea" />
+                </div>
+                <div className="flex w-24">Opensea link:</div>
+              </div>
+
+              <div className="flex flex-row flex-1 items-center truncate">
+                <div className="flex flex-1 truncate">
+                  <a
+                    className="link link-primary truncate min-w-0"
+                    href={openseaUrl}
+                    target="_blank"
+                    rel="noreferrer">
+                    {openseaUrl}
+                  </a>
+                </div>
+
+                <div data-tip="Copy opensea link" className="tooltip tooltip-left">
+                  <Button
+                    class="btn btn-square btn-accent text-primary-content"
+                    onClick={() => copyShareUrl(openseaUrl)}>
+                    <div className="relative w-5 h-5">
+                      <Image src={CopyImg} alt="copy" layout="fill" />
+                    </div>
+                  </Button>
+                </div>
+              </div>
+            </div>
+          </div>
+
           <div className="flex-col md:flex-row flex gap-5 mt-5">
             <Link href={`/m/${lastMintedId}`}>
               <a className="flex flex-1">
-                <Button className="btn-primary flex flex-1">Go to image page</Button>
+                <Button className="btn-primary flex flex-1">Go to meme page</Button>
               </a>
             </Link>
 
--- src/constants.ts
+++ src/constants.ts
@@ -5,12 +5,11 @@ export const DISCORD_URL = 'https://discord.gg/';
 export const TWITTER_URL = 'https://twitter.com/';
 export const INSTAGRAM_URL = 'https://www.instagram.com/';
 
-export const TOKEN_SYMBOL = '$BLESS';
 export const ETH_SYMBOL = 'ETH';
-
-export const MAX_TO_MINT = 10;
 export const IPFS_IMAGE_BASE = 'https://gateway.pinata.cloud/ipfs/';
 
 export const MAX_IMAGE_SIZE = 1000;
 //use secured one for prod
 export const PUBLIC_RPC = 'https://rinkeby.infura.io/v3/998804b23fd0421e9bbea7c41917c2b3';
+export const MEME_PATH = 'm/';
+export const OPENSEA_BASE = 'https://testnets.opensea.io/assets/';
