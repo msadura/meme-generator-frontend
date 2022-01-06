3fd8aa131a12a95c95e35832cbcd72753f097517
Marek Sadura <m.sadura@outlook.com>
2022-01-06 10:52:47 +0100

    Fix tsc

--- src/utils/getMintCurrencyLabel.ts
+++
@@ -1,5 +0,0 @@
-import { TOKEN_SYMBOL, ETH_SYMBOL } from '@app/constants';
-
-export function getMintCurrencyLabel(currentCount: number, maxPaid: number) {
-  return currentCount >= maxPaid ? TOKEN_SYMBOL : ETH_SYMBOL;
-}
