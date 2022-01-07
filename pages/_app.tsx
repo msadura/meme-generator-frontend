b76b4b3acb4ee1546e2e666251d113dfb5bcb3bb
Marek Sadura <m.sadura@outlook.com>
2022-01-07 20:58:02 +0100

    test provider

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -5,18 +5,15 @@ import { BlockchainProvider } from '@app/blockchain/BlockchainContext';
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 import { PublicProvider } from '@app/components/PublicProvider/PublicProvider';
-import { CanvasProvider } from '@app/components/Canvas/CanvasProvider';
 import { MemeProvider } from '@app/components/MemeProvider/MemeProvider';
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <PublicProvider>
       <BlockchainProvider>
-        <CanvasProvider>
-          <MemeProvider>
-            <Component {...pageProps} />
-          </MemeProvider>
-        </CanvasProvider>
+        <MemeProvider>
+          <Component {...pageProps} />
+        </MemeProvider>
 
         <ToastContainer />
       </BlockchainProvider>
