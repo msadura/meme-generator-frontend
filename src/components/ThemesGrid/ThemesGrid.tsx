90be17f405457328f4c1866b437e9d1876d9157d
Marek <m.sadura@outlook.com>
2022-01-12 09:26:15 +0100

    Add favicon, easy masonery grid

--- src/components/ThemesGrid/ThemesGrid.tsx
+++ src/components/ThemesGrid/ThemesGrid.tsx
@@ -10,45 +10,11 @@ type Props = {
 };
 
 export function ThemesGrid({ themes, onSelect }: Props): JSX.Element | null {
-  const [containerWidth, setContainerWidth] = useState(0);
   const containerRef = useRef<HTMLDivElement>(null);
   const [gridThemes, setGridThemes] = useState(themes);
 
-  const measureContainer = useRef(() => {
-    const width = containerRef.current?.clientWidth;
-    setContainerWidth(width || 0);
-  }).current;
-
-  const Grid = useMemo(() => {
-    if (!containerWidth) {
-      return null;
-    }
-
-    return makeResponsive(measureItems(CSSGrid, { measureImages: true }), {
-      maxWidth: containerWidth,
-      minPadding: 0
-    });
-  }, [containerWidth]);
-
   useEffect(() => {
-    if (themes.length) {
-      measureContainer();
-    }
-  }, [measureContainer, themes.length]);
-
-  useEffect(() => {
-    window.addEventListener('resize', measureContainer);
-    () => window.removeEventListener('resize', measureContainer);
-  }, [measureContainer]);
-
-  useEffect(() => {
-    if (Grid) {
-      setTimeout(() => setGridThemes(shuffle(themes)));
-    }
-  }, [Grid, themes]);
-
-  useEffect(() => {
-    setGridThemes(themes);
+    setGridThemes(shuffle(themes));
   }, [themes]);
 
   if (!themes.length) {
@@ -60,29 +26,11 @@ export function ThemesGrid({ themes, onSelect }: Props): JSX.Element | null {
       <p className="text-accent-focus text-lg mb-3">
         Need inspiration? Pick some of trending themes:
       </p>
-      {Grid && (
-        <Grid
-          component="ul"
-          columns={4}
-          columnWidth={150}
-          gutterWidth={0}
-          gutterHeight={0}
-          layout={layout.pinterest}
-          duration={800}
-          easing="ease-out">
-          {gridThemes.map((t) => (
-            <li
-              key={t.id}
-              className="relative"
-              style={{
-                width: 150,
-                height: 'auto'
-              }}>
-              <ThemeItem item={t} onClick={onSelect} />
-            </li>
-          ))}
-        </Grid>
-      )}
+      <div className="masonry sm:masonry-sm md:masonry-md md:masonry-lg">
+        {gridThemes.map((t) => (
+          <ThemeItem key={t.id} item={t} onClick={onSelect} />
+        ))}
+      </div>
     </div>
   );
 }
