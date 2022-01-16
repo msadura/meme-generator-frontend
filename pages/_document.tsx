550fb247431541fef650dd613155919ee53245b7
Marek <m.sadura@outlook.com>
2022-01-16 19:26:47 +0100

    load impact font

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -16,6 +16,7 @@ class ExtendedDocument extends Document {
             href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,200;0,300;0,400;0,700;1,300;1,400&display=swap"
             rel="stylesheet"
           />
+          <link rel="preload" href="/fonts/impact.ttf" as="font" crossOrigin="" />
         </Head>
         <body className="bit text-xs md:text-sm">
           <Main />
