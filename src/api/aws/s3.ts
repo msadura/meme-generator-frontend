ccd3bddbf9d91a1ec5e65321ef190b5fc3da68d0
Marek <m.sadura@outlook.com>
2022-02-17 22:10:10 +0100

    update aws config

--- src/api/aws/s3.ts
+++ src/api/aws/s3.ts
@@ -2,10 +2,16 @@ import { AWS_BUCKET } from '@app/api/constants';
 import { FILE_EXTENSION } from '@app/constants';
 import AWS from 'aws-sdk';
 
-export const s3 = new AWS.S3({
-  accessKeyId: process.env.AWS_KEY,
-  secretAccessKey: process.env.AWS_SECRET
+AWS.config.update({
+  credentials: {
+    accessKeyId: process.env.AWS_KEY as string,
+    secretAccessKey: process.env.AWS_SECRET as string
+  }
 });
+
+export const s3 = new AWS.S3();
+
+console.log('🔥conf', process.env.AWS_KEY, process.env.AWS_SECRET);
 
 export const uploadS3 = async (body: string | Buffer, filename: string) => {
   const params = {
